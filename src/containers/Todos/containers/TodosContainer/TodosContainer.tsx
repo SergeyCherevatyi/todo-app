f5ed547354c6110e1ef09172e724c3f09933f22b
Serhii Cherevatyi <sergeycherevatyi@gmail.com>
2021-09-20 17:13:11 +0300

    Init app

---
+++ src/containers/Todos/containers/TodosContainer/TodosContainer.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+//import styles from './index.module.scss'
+import { Header, Footer, Filters, Todos } from "@containers/";
+
+interface IComponentProps {
+  children?: React.ReactNode;
+}
+
+const TodosContainer = (props: IComponentProps) => {
+  return (
+    <div>
+      <Header />
+      <Todos />
+      <Filters />
+      <Footer />
+    </div>
+  );
+};
+
+// class TodosContainer extends React.Component<IComponentProps> {
+//   render() {
+//     return (
+//       <div>
+//         <Header />
+//         <Todos />
+//         <Filters />
+//         <Footer />
+//       </div>
+//     );
+//   }
+// }
+
+export default TodosContainer;
