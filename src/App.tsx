9b7022939290d6db994bdb724cebe35988fd16e1
Serhii Cherevatyi <sergeycherevatyi@gmail.com>
2021-09-22 22:35:04 +0300

    Practice passing props

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import { TodosContainer } from "@containers/";
+import {ITodo} from "./containers/Todos/components/Todo";
 
-const App = () => <TodosContainer />;
+
+
+const App = () => {
+
+  const [todos] = useState<ITodo[]>([{
+    id: 1,
+    text: "Test 001",
+    createAt: new Date(),
+    completed: false,
+  },
+    {
+      id: 2,
+      text: "Test 002",
+      createAt: new Date(),
+      completed: true,
+    }
+
+  ])
+
+  return <TodosContainer todos={todos} />;
+
+}
+
+
+
 
 export default App;
